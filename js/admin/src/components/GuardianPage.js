aada08d98f3c020ea77f12ae3ec91af5b2c910c6
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-12-16 14:49:47 +0100

    updated relation to trans

--- js/admin/src/components/GuardianPage.js
+++ js/admin/src/components/GuardianPage.js
@@ -20,7 +20,7 @@ function userItem(user) {
             m('a', {href: app.forum.attribute('baseUrl') + '/u/' + user.username()}, user.username())
         ]),
         m('td', humanTime(user.joinTime())),
-        m('td', user.lastSeenTime() ? humanTime(user.lastSeenTime()) : app.translator.trans('hyn-guardian.generic.never')),
+        m('td', user.lastSeenTime() ? humanTime(user.lastSeenTime()) : app.translator.trans('hyn-guardian.admin.user.states.never_visited')),
         m('td', user.isActivated() ? icon('check') : icon('close')),
         m('td', user.badges().toArray().length ? m('ul', {className: 'UserCard-badges badges'}, listItems(user.badges().toArray())) : ''),
         m('td', [
