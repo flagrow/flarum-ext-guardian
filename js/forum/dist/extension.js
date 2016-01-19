a6ba11f21de8012f84f530d81bb934912c316340
Daniel Klabbers <daniel+hyn@klabbers.email>
2016-01-19 20:39:17 +0100

    added ip from dropdown

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -1,28 +1,30 @@
-System.register('hyn/guardian/main', ['flarum/extend', 'flarum/components/PostMeta', 'flarum/components/CommentPost', 'flarum/models/Post', 'flarum/Model'], function (_export) {
+System.register('hyn/guardian/main', ['flarum/extend', 'flarum/components/CommentPost', 'flarum/models/Post', 'flarum/Model', 'flarum/helpers/icon'], function (_export) {
     'use strict';
 
-    var extend, PostMeta, CommentPost, Post, Model;
+    var extend, CommentPost, Post, Model, icon;
     return {
         setters: [function (_flarumExtend) {
             extend = _flarumExtend.extend;
-        }, function (_flarumComponentsPostMeta) {
-            PostMeta = _flarumComponentsPostMeta['default'];
         }, function (_flarumComponentsCommentPost) {
             CommentPost = _flarumComponentsCommentPost['default'];
         }, function (_flarumModelsPost) {
             Post = _flarumModelsPost['default'];
         }, function (_flarumModel) {
             Model = _flarumModel['default'];
+        }, function (_flarumHelpersIcon) {
+            icon = _flarumHelpersIcon['default'];
         }],
         execute: function () {
 
             app.initializers.add('hyn-guardian', function (app) {
+                // extend the Post object to add the user_ip
                 babelHelpers._extends(Post.prototype, {
                     user_ip: Model.attribute('user_ip')
                 });
+                // extend the CommentPost
                 extend(CommentPost.prototype, 'headerItems', function (items) {
                     if (this.props.post.user_ip()) {
-                        items.add('guardian', m('div', { className: 'PostGuardian PostMeta' }, [this.props.post.user_ip()]));
+                        items.add('guardian', m('div', { className: 'PostGuardian PostMeta Dropdown' }, [m('a', { className: 'Dropdown-toggle', 'data-toggle': 'dropdown' }, [icon('user-secret')]), m('div', { className: 'Dropdown-menu dropdown-menu' }, [m('span', [icon('wifi'), this.props.post.user_ip()])])]));
                     }
 
                     return items;
--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -1,17 +1,27 @@
 import { extend } from 'flarum/extend';
-import PostMeta from 'flarum/components/PostMeta';
 import CommentPost from 'flarum/components/CommentPost';
 import Post from 'flarum/models/Post';
 import Model from 'flarum/Model';
+import icon from 'flarum/helpers/icon';
 
 app.initializers.add('hyn-guardian', app => {
+    // extend the Post object to add the user_ip
     Object.assign(Post.prototype, {
         user_ip: Model.attribute('user_ip')
     });
+    // extend the CommentPost
     extend(CommentPost.prototype, 'headerItems', function(items) {
         if(this.props.post.user_ip()) {
-            items.add('guardian', m('div', {className: 'PostGuardian PostMeta'}, [
-                this.props.post.user_ip()
+            items.add('guardian', m('div', {className: 'PostGuardian PostMeta Dropdown'}, [
+                m('a', {className: 'Dropdown-toggle', 'data-toggle': 'dropdown'}, [
+                    icon('user-secret')
+                ]),
+                m('div', {className: 'Dropdown-menu dropdown-menu'}, [
+                    m('span', [
+                        icon('wifi'),
+                        this.props.post.user_ip()
+                    ])
+                ])
             ]));
         }
 
