b0a37778412e8619c964f7bb918d3aae57f26b64
Luceos <daniel+github@klabbers.email>
2016-01-20 01:06:01 -0500

    Applied fixes from StyleCI

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of flagrow/flarum-ext-guardian.
+ *
+ * Copyright (c) Flagrow.
+ *
+ * http://flagrow.github.io
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
 namespace Flagrow\Guardian;
 
 use Illuminate\Contracts\Events\Dispatcher;
